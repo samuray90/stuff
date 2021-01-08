26066101d17f83dc31b6de2393fd60a2427485fa
samuray90 <ionutsdaniel53@gmail.com>
2021-01-08 14:23:07 +0200

    state

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 
 const MovieList = () => {
-  const [movies, setMovies] =useState([
+  const [movies, setMovies] = useState([
     {
-      name:"Harry Potter",
+      name: "Harry Potter",
       price: "£90",
-      id: 96
+      id: 96,
     },
     {
-      name:"Harry Potter",
+      name: "Harry Potter",
       price: "£90",
-      id: 96
+      id: 96,
     },
     {
-      name:"Harry Potter",
+      name: "Harry Potter",
       price: "£90",
-      id: 96
-    }
+      id: 96,
+    },
   ]);
   return (
-
+    <div>
+      {movies.map((movie) => (
+        <li>{movie.name}</li>
+      ))}
+    </div>
   );
 };
 
