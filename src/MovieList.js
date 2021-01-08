a04ee69d547b4bc6dee554342e1285a47d89af0a
samuray90 <ionutsdaniel53@gmail.com>
2021-01-08 14:07:06 +0200

    the commit

---
+++ src/MovieList.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const MovieList = () => {
+  const [movies, setMovies] =useState([
+    {
+      name:"Harry Potter",
+      price: "£90",
+      id: 96
+    },
+    {
+      name:"Harry Potter",
+      price: "£90",
+      id: 96
+    },
+    {
+      name:"Harry Potter",
+      price: "£90",
+      id: 96
+    }
+  ]);
+  return (
+
+  );
+};
+
+export default MovieList;
+
+//rafce
